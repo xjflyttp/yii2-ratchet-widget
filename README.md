8c388a08eb6fe8abe713833c7d1bd48a31c58f42
xjflyttp <xjflyttp@gmail.com>
2015-08-19 11:59:18 +0800

    add TabBar

--- README.md
+++ README.md
@@ -1,4 +1,51 @@
 # yii2-ratchet-widget
+    https://github.com/twbs/ratchet
+
+
+## composer.json
+```json
+"require": {
+    "yii2-ratchet-widget": "~1.0.0"
+}
 ```
-https://github.com/twbs/ratchet
+
+## RegisterAssets
+### Base
+```php
+RatchetAsset::register($this);
+```
+### AndroidTheme
+```php
+RatchetAndroidAsset::register($this);
+```
+### IosTheme
+```php
+RatchetIosAsset::register($this);
+```
+
+## Widget TabBar
+### Icon
+```php
+echo TabBar::widget([
+    'items' => [
+        ['label' => '首页', 'url' => ['shop/index'], 'icon' => 'icon icon-home'],
+        ['label' => '最新', 'url' => ['shop/hot'], 'icon' => 'icon icon-search'],
+        ['label' => '热门', 'url' => ['shop/hot'], 'icon' => 'icon icon-star-filled'],
+        ['label' => '没想好', 'url' => ['shop/hot'], 'icon' => 'icon icon-gear'],
+        ['label' => '订单', 'url' => ['profile/order'], 'icon' => 'icon icon-person'],
+    ]
+]);
+```
+
+### Label Only
+```php
+echo TabBar::widget([
+    'items' => [
+        ['label' => '首页', 'url' => ['shop/index']],
+        ['label' => '最新', 'url' => ['shop/hot']],
+        ['label' => '热门', 'url' => ['shop/hot']],
+        ['label' => '没想好', 'url' => ['shop/hot']],
+        ['label' => '订单', 'url' => ['profile/order']],
+    ]
+]);
 ```
---
+++ widgets/TabBar.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace xj\ratchet\widgets;
+
+use xj\ratchet\RatchetAsset;
+use Yii;
+use yii\base\InvalidConfigException;
+use yii\base\Widget;
+use yii\helpers\ArrayHelper;
+use yii\helpers\Html;
+
+/**
+ * @author xjflyttp <xjflyttp@gmail.com>
+ */
+class TabBar extends Widget
+{
+    public $options = ['class' => 'bar bar-tab'];
+    public $items = [];
+    public $encodeLabels = true;
+    public $activateItems = true;
+    public $route;
+    public $params;
+
+    public function init()
+    {
+        parent::init();
+        if ($this->route === null && Yii::$app->controller !== null) {
+            $this->route = Yii::$app->controller->getRoute();
+        }
+        if ($this->params === null) {
+            $this->params = Yii::$app->request->getQueryParams();
+        }
+    }
+
+    public function run()
+    {
+        RatchetAsset::register($this->getView());
+        return $this->renderItems();
+    }
+
+    /**
+     * Renders widget items.
+     */
+    public function renderItems()
+    {
+        $items = [];
+        foreach ($this->items as $i => $item) {
+            if (isset($item['visible']) && !$item['visible']) {
+                continue;
+            }
+            $items[] = $this->renderItem($item);
+        }
+        return Html::tag('nav', implode("\n", $items), $this->options);
+    }
+
+    /**
+     * Renders a widget's item.
+     * @param string|array $item the item to render.
+     * @return string the rendering result.
+     * @throws InvalidConfigException
+     */
+    public function renderItem($item)
+    {
+        if (is_string($item)) {
+            return $item;
+        }
+        if (!isset($item['label'])) {
+            throw new InvalidConfigException("The 'label' option is required.");
+        }
+        $encodeLabel = isset($item['encode']) ? $item['encode'] : $this->encodeLabels;
+        $label = $encodeLabel ? Html::encode($item['label']) : $item['label'];
+        $url = ArrayHelper::getValue($item, 'url', '#');
+        $linkOptions = ArrayHelper::getValue($item, 'linkOptions', ['class' => 'tab-item']);
+
+        $iconTag = isset($item['icon']) ? Html::tag('span', '', ['class' => $item['icon']]) : '';
+        $labelTag = isset($item['icon']) ? Html::tag('span', $label, ['class' => 'tab-label']) : $label;
+
+        if (isset($item['active'])) {
+            $active = ArrayHelper::remove($item, 'active', false);
+        } else {
+            $active = $this->isItemActive($item);
+        }
+        if ($this->activateItems && $active) {
+            Html::addCssClass($linkOptions, 'active');
+        }
+        return Html::a($iconTag . $labelTag, $url, $linkOptions);
+    }
+
+
+    /**
+     * Checks whether a menu item is active.
+     * This is done by checking if [[route]] and [[params]] match that specified in the `url` option of the menu item.
+     * When the `url` option of a menu item is specified in terms of an array, its first element is treated
+     * as the route for the item and the rest of the elements are the associated parameters.
+     * Only when its route and parameters match [[route]] and [[params]], respectively, will a menu item
+     * be considered active.
+     * @param array $item the menu item to be checked
+     * @return boolean whether the menu item is active
+     */
+    protected function isItemActive($item)
+    {
+        if (isset($item['url']) && is_array($item['url']) && isset($item['url'][0])) {
+            $route = $item['url'][0];
+            if ($route[0] !== '/' && Yii::$app->controller) {
+                $route = Yii::$app->controller->module->getUniqueId() . '/' . $route;
+            }
+            if (ltrim($route, '/') !== $this->route) {
+                return false;
+            }
+            unset($item['url']['#']);
+            if (count($item['url']) > 1) {
+                $params = $item['url'];
+                unset($params[0]);
+                foreach ($params as $name => $value) {
+                    if ($value !== null && (!isset($this->params[$name]) || $this->params[$name] != $value)) {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+}
